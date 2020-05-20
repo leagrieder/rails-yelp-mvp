425206f19e2fbc135651813d18d685ab24f31a9b
Lea Grieder <lea@grieder.org>
2020-05-20 15:09:08 +0200

    m

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  CATEGORY = '%w'['chinese', 'italian', 'japanese', 'french', 'belgian']
+  CATEGORY = %W[chinese italian japanese french belgian]
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 Restaurant.destroy_all
 
 one = { name: 'Noix de Coco', address: 'Vevey',
-        phone_number: '±41 79 136 78 17', categroy: 'chinese' }
+        phone_number: '±41 79 136 78 17', category: 'chinese' }
 
 two = { name: 'Siamo Forno', address: 'Buenos Aires',
         phone_number: '+54 11 6374 31 22', category: 'italian' }

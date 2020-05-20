6324ba5ee57b72494a04c5e681d003672db354cd
Lea Grieder <lea@grieder.org>
2020-05-20 13:00:24 +0200

    m

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  CATEGORY = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  CATEGORY = '%w'['chinese', 'italian', 'japanese', 'french', 'belgian']
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+one = { name: 'Noix de Coco', address: 'Vevey',
+        phone_number: '±41 79 136 78 17', categroy: 'chinese' }
+
+two = { name: 'Siamo Forno', address: 'Buenos Aires',
+        phone_number: '+54 11 6374 31 22', category: 'italian' }
+
+three = { name: 'Buenos Aires Verde', address: 'Palermo',
+          phone_number: '+54 11 2345 31 22',
+          category: 'belgian' }
+
+four = { name: 'el niño gordo', address: 'Palermo Soho',
+         phone_number: '+54 11 8777 21 29',
+         category: 'japanese' }
+
+five = { name: 'Chez Gustau\'s', address: 'Ratatouille',
+         phone_number: '+000000000', category: 'french' }
+
+[one, two, three, four, five].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
